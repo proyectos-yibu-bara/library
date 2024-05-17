97a8e2eaa539b7b7492f35e02bfbe8aaeed5162b
jfbarahonag <jfbarahonag@unal.edu.co>
2024-05-16 22:56:27 -0500

    category controller renamed

--- src/routes/category.ts
+++ src/routes/category.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import * as controller from "../controllers/category";
+import * as controller from "../controllers/categoryController";
 
 export const categoriesRouter = Router();
 
